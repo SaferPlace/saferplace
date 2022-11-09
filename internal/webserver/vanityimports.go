8523f01c7bc0941fa27b1ee0161f33cacc05fa10
Wojtek Bednarzak <wojtek.bednarzak@gmail.com>
2022-11-09 13:14:54 +0000

    Local variables for vanity imports
    It was somehow missed by the linters and testing
    
    Change-Id: I122c37119c7df4e0e2e48a9c199b6cc8cd63541b

--- internal/webserver/vanityimports.go
+++ internal/webserver/vanityimports.go
@@ -2,6 +2,7 @@ package webserver
 
 var vanityImports = map[string]string{
 	"safer.place":           "saferplace",
+	"api.safer.place":       "api",
 	"eircode.safer.place":   "eircode",
 	"ingest.safer.place":    "ingest",
 	"review.safer.place":    "review",
