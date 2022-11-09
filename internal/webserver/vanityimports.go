e237206e2bd3ab98fbda4c2680453d888fb516f6
Wojtek Bednarzak <wojtek.bednarzak@gmail.com>
2022-11-09 14:50:48 +0000

    Remove safer.place vanity import
    There would not be a case where the safer.place packages are imported.
    
    Change-Id: I4e859daf45e7fa264a1658cceb8721886534050e

--- internal/webserver/vanityimports.go
+++ internal/webserver/vanityimports.go
@@ -1,7 +1,6 @@
 package webserver
 
 var vanityImports = map[string]string{
-	"safer.place":           "saferplace",
 	"api.safer.place":       "api",
 	"eircode.safer.place":   "eircode",
 	"ingest.safer.place":    "ingest",
