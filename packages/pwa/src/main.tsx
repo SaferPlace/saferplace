76953cb98e8072c9f65a090e259b1ba942b19489
Wojtek Bednarzak <wojtek.bednarzak@gmail.com>
2023-08-15 01:59:05 +0100

    Typescript CI
    Add Typescript CI which will only trigger if the npm packages have
    changed.
    
    Also included is hopefully dependabot being able to find packages.

--- packages/pwa/src/main.tsx
+++ packages/pwa/src/main.tsx
@@ -4,7 +4,7 @@ import {
   createBrowserRouter,
   RouterProvider
 } from 'react-router-dom'
-import Root from './routes/Root'
+import Root from './routes/root'
 
 const router = createBrowserRouter([
   {
