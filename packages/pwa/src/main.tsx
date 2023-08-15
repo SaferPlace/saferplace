620689affae84e0d3fc990231130fcb48901d25d
Wojtek Bednarzak <wojtek.bednarzak@gmail.com>
2023-08-15 01:15:04 +0100

    pwa: Boilerplate (#46)
    Add the PWA boilerplate so we can start building functionality on top of
    it.

---
+++ packages/pwa/src/main.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+import Root from './routes/Root'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
