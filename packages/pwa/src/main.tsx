aa857d27f3be1bd6ec0051923df64f8bb0197fcf
Wojtek Bednarzak <wojtek.bednarzak@gmail.com>
2023-08-16 01:13:17 +0100

    pwa: Landing Page and Login
    Add a Landing page and login page. For now the page just accepts a email
    as a plain string without any verification. This is just so we can have
    something working without the need to implement authentication.

--- packages/pwa/src/main.tsx
+++ packages/pwa/src/main.tsx
@@ -5,16 +5,23 @@ import {
   RouterProvider
 } from 'react-router-dom'
 import Root from './routes/root'
+import { CssBaseline } from '@mui/material'
+import Login from './routes/login'
+import './i18n'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+  }, {
+    path: '/login',
+    element: <Login />,
   }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <CssBaseline />
   </React.StrictMode>,
 )
