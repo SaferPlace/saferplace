ecaf04ceb29d4456f258abc83cb27017a44e4826
Wojtek Bednarzak <wojtek.bednarzak@gmail.com>
2023-08-17 02:12:39 +0100

    Trigger PWA deployment
    Test Cloudflare building from CI.

--- packages/pwa/src/routes/incident/single.tsx
+++ packages/pwa/src/routes/incident/single.tsx
@@ -8,6 +8,7 @@ export default function IncidentDetails() {
     return (
         <Card>
             {incident.description}
+            {incident.timestamp?.toDate.toString()}
         </Card>
     )
 }
