694b5077d1516f76352d17c6aba7658928ff8260
Wojtek Bednarzak <wojtek.bednarzak@gmail.com>
2023-08-30 00:55:55 +0100

    Allow the reviewer to specify their email (#140)
    This is completely not authentication but it at least can be used with
    proper backend middleware to extract the email of the reviewer.

--- packages/review-ui/src/routes/root.tsx
+++ packages/review-ui/src/routes/root.tsx
@@ -3,17 +3,33 @@ import {
   AppBar,
   Box,
   Container,
+  TextField,
   Toolbar,
   Typography,
 } from '@mui/material'
 import { Outlet } from 'react-router-dom'
 
 export default function Root() {
+  const [ email, setEmail ] = React.useState<string>(localStorage.getItem('email') ?? '')
+
+  const updateEmail = (value: string) => {
+    localStorage.setItem('email', value)
+    setEmail(value)
+  }
+
   return (
     <Box>
       <AppBar position='fixed'>
-        <Toolbar>
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between'}}>
           <Typography variant='h4'>SaferPlace Review</Typography>
+          <Box>
+          <TextField
+            variant='outlined'
+            value={email}
+            onChange={e => updateEmail(e.target.value)}
+            placeholder='Reviewer Email'
+          />
+          </Box>
         </Toolbar>
       </AppBar>
       <Container>
